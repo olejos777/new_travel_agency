3734031ed53b7370d9fc14ff48001ab72426590b
olejos777 <olejniczakk.maciejj@gmail.com>
2020-07-06 19:21:31 +0200

    14.5 add duration change and filtering by tags

--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,16 +1,20 @@
 import {connect} from 'react-redux';
 import TripListOptions from './TripListOptions';
 import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import {getAllFilters, changeSearchPhrase, addTag, removeTag, changeTripDuration} from '../../../redux/filtersRedux';
 
 const mapStateToProps = state => ({
   tags: getAllTags(state),
   filters: getAllFilters(state),
+
 });
 
 const mapDispatchToProps = dispatch => ({
   changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
   // TODO - add more dispatchers for other filters
+  changeTripDuration: (type, value) => dispatch(changeTripDuration(type, value)),
+  addTag: tag => dispatch(addTag(tag)),
+  removeTag: tag => dispatch(removeTag(tag)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);

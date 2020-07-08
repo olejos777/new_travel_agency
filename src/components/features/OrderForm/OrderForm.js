995abff1768044bb87f29db6c3271802fa980efd
olejos777 <olejniczakk.maciejj@gmail.com>
2020-07-08 22:06:45 +0200

    15.2 fix display trip price

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 
-import { Row, Col, Grid } from 'react-flexbox-grid';
+const OrderForm = (props) => (
+  <Grid>
+    <Row>
+      <Col xs={12}>
+        <OrderSummary cost={props.tripCost} options={props.options} />
+      </Col>
+    </Row>
+  </Grid>
+);
 
-class OrderForm extends React.Component {
-  static propTypes = {
-    options: PropTypes.object,
-    tripCost: PropTypes.string,
-  }
-
-  render() {
-    const { options, tripCost } = this.props;
-    return (
-      <Grid>
-        <Row>
-          <Col xs={12}>
-            <OrderSummary cost={tripCost} options={options} />
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
-}
+OrderForm.propTypes = {
+  options: PropTypes.object,
+  tripCost: PropTypes.string,
+};
 
 export default OrderForm;

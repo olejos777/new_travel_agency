8338323f9c67ab36bc9b81265e05e6a71dc4693b
olejos777 <olejniczakk.maciejj@gmail.com>
2020-07-11 21:07:53 +0200

    15.3 add OrderOption subcomponents

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,10 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = (props) => (
   <Grid>
     <Row>
+      {pricing.map(option => (
+        <Col md={4} key={option.id}>
+          <OrderOption {...option} >
+
+          </OrderOption>
+        </Col>
+      ))};
       <Col xs={12}>
         <OrderSummary costing={props.tripCosting} options={props.options} />
       </Col>
---
+++ src/components/features/OrderOption/OrderOption.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+
+import OrderOptionDropdown from './OrderOptionDropdown';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionNumber from './OrderOptionNumber';
+
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  checkboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+
+const OrderOption = ({ name, type, ...otherProps }) => {
+  //console.log('otherProps to:', ...otherProps);
+  const OptionComponent = optionTypes[type];
+  if (!OptionComponent) {
+    return null;
+  } else {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>
+          {name}
+        </h3>
+        <OptionComponent
+          {...otherProps}
+        />
+      </div>
+    );
+  }
+};
+
+OrderOption.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  id: PropTypes.string,
+  setOrderOption: PropTypes.func,
+};
+
+export default OrderOption;
+
